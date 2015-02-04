27574505d811eb28a078fad582f58cc1255ee282
lovelyelephant <Anthony.Nichols@yale.edu>
2015-02-04 01:37:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send(index.html);
 });
 
 app.listen(process.env.PORT || 4000);
