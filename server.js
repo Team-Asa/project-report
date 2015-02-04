8b0c2aa5fb9ea5a2ace9b592495feae18640abb6
Anthony Nichols <anthony.nichols@yale.edu>
2015-02-04 06:56:55 +0000

    fixed the response.send

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send(index.html);
+    response.send('index.html');
 });
 
 app.listen(process.env.PORT || 4000);
