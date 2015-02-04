048068aed5607d6256a1db09026ffe8e512460dc
lovelyelephant <Anthony.Nichols@yale.edu>
2015-02-04 01:27:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,4 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.get('/nickname', function(request, response){
-    response.send('lovely elephant');
-});
-
 app.listen(process.env.PORT || 4000);
