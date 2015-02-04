a8c12b074f744dc13e46f9b09b16f79a705fd87f
Anthony Nichols <anthony.nichols@yale.edu>
2015-02-04 07:25:12 +0000

    still trying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.render('index.html');
+    response.sendfile('index.html');
 });
 
 app.listen(process.env.PORT || 4000);
